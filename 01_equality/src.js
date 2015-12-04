4ae522f4cdf315a6c3582376e245baf86c95d2d0
Maria <mmorrison1978@gmail.com>
2015-12-04 10:29:31 -0700

    finished

--- 01_equality/src.js
+++ 01_equality/src.js
@@ -1,5 +1,27 @@
 module.exports = {
-  sameLength: function(string1, string2) {
-    // your code here:
-  }
+  sameLength: function(s1, s2) {
+    var results = (s1.length == s2.length) ? true:false;
+    return results;
+  },
+
+  firstGreater: function(a1, a2) {
+    var results = (a1 > a2) ? true:false;
+    return results;
+  },
+
+  firstGreaterOrEqualTo: function(a1, a2) {
+    var results = (a1 >= a2) ? true:false;
+    return results;
+  },
+
+  firstLengthGreater: function (a1, a2) {
+    var results = (a1.length > a2.length) ? true:false;
+    return results;
+  },
+
+  secondGreater: function (a1, a2) {
+    var results = (a2 > a1) ? true:false;
+    return results;
+  },
+
 }
